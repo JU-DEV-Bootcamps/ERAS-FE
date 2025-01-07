ca4f376e93d7be961fbd38704490a0b2d728ebea
Mauricio Huayta <huaytamauricio1@gmail.com>
2025-01-07 11:36:36 -0400

    Add angular material module and create preview component

---
+++ src/app/theme-preview/theme-preview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-preview',
+  imports: [],
+  templateUrl: './theme-preview.component.html',
+  styleUrl: './theme-preview.component.css'
+})
+export class ThemePreviewComponent {
+
+}
