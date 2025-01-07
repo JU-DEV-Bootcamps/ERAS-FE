385e032be523c8b7dc5eb3f56824ddd46e7e652a
Mauricio Huayta <huaytamauricio1@gmail.com>
2025-01-07 14:18:12 -0400

    Move example component to correct folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { ThemePreviewComponent } from './theme-preview/theme-preview.component';
+import { ThemePreviewComponent } from './shared/components/theme-preview/theme-preview.component';
 
 export const routes: Routes = [
   { path: 'preview', component: ThemePreviewComponent },
