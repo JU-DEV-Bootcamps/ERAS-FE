d67adfceda3beb265d973c46e492744a44186576
Mauricio Huayta <huaytamauricio1@gmail.com>
2025-01-07 11:53:53 -0400

    Configure custom theme and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ThemePreviewComponent } from './theme-preview/theme-preview.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'preview', component: ThemePreviewComponent },
+];
