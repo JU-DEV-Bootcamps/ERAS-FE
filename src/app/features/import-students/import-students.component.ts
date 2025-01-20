2c57f46568e80953cf3e028896745a32d7880559
Mauricio Huayta <huaytamauricio1@gmail.com>
2025-01-20 11:44:05 -0400

    feat: New component for imports csv students view

---
+++ src/app/features/import-students/import-students.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-import-students',
+  imports: [MatButtonModule],
+  templateUrl: './import-students.component.html',
+  styleUrl: './import-students.component.css',
+})
+export class ImportStudentsComponent {}
