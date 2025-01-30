2ab00c0d952210b7f8f6265d4110d4eaf7b9166d
Mauricio Huayta <huaytamauricio1@gmail.com>
2025-01-30 09:47:14 -0400

    refactor: Messages for import success, error and validation moved to file core/constants/messages.ts and used in the import components

---
+++ src/app/core/constants/messages.ts
@@ -0,0 +1,21 @@
+export const IMPORT_MESSAGES = {
+  ANSWERS_SUCCESS:
+    'The survey’s answers were saved in the system successfully.',
+  ANSWERS_ERROR:
+    'There was an error with the import, please try again or check the values.',
+  STUDENT_SUCCESS:
+    'The students information was saved into the system successfully.',
+  STUDENT_ERROR:
+    'There was an error during the import process, please try again.',
+};
+
+export const VALIDATION_MESSAGES = {
+  FILE_SIZE_EXCEEDED: 'File size exceeds the maximum limit.',
+  INVALID_FILE_TYPE: 'Invalid file type, please select a correct file.',
+  CSV_SCAN_ERROR: "CSV Scan: Following errors detected in the file's content.",
+};
+
+export const GENERAL_MESSAGES = {
+  SUCCESS_TITLE: 'Successful',
+  ERROR_TITLE: 'Warning',
+};
