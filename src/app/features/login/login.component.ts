99f0a19c06eda3b9a5bcb4206a6302a117b76548
Hap Rojas <adrian.rojas@abe.bo>
2025-02-04 09:45:27 -0400

    Back to original icon

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { KeycloakAuthComponent } from '../../shared/components/keycloak-auth/keycloak-auth.component';
 import { KeycloakService } from '../../core/services/keycloak.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
-  imports: [KeycloakAuthComponent],
+  imports: [KeycloakAuthComponent, MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
