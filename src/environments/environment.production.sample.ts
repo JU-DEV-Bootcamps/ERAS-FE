03529541b7eff2330bdd3f9b85a22c1152b395ca
Mauricio Huayta <huaytamauricio1@gmail.com>
2025-02-07 12:45:31 -0400

    fix: Fixed environments files and workflow to create the image locally or on workflow

--- src/environments/environment.production.sample.ts
+++ src/environments/environment.production.sample.ts
@@ -1,3 +1,9 @@
+/**
+ * To create the image of this project locally, make sure to:
+ * 1. Create the file environment.production.ts in the same folder.
+ * 2. Correctly set your local environment variables (copy the ones from the development environment).
+ */
+
 export const environment = {
   production: false,
   apiUrl: '__API_URL__',
