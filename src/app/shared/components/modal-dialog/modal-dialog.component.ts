09242c20358f9d57abe95ce4742bf4c95eede7dd
Israel <israelquisbert.dev@gmail.com>
2025-02-13 20:15:27 -0400

    shared: Modal dialog created to be used across the project

---
+++ src/app/shared/components/modal-dialog/modal-dialog.component.ts
@@ -0,0 +1,48 @@
+import { NgClass, NgFor } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialogActions,
+  MatDialogContent,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+import { MatIcon } from '@angular/material/icon';
+
+export interface DialogData {
+  title: string;
+  message: string;
+  isSuccess: boolean;
+  success: {
+    details: string;
+  };
+  error: {
+    title: string;
+    message: string;
+    showMessage: boolean;
+    details: string[];
+  };
+}
+@Component({
+  selector: 'app-modal',
+  imports: [
+    MatButtonModule,
+    MatDialogActions,
+    MatDialogContent,
+    MatIcon,
+    NgFor,
+    NgClass,
+  ],
+  templateUrl: './modal-dialog.component.html',
+  styleUrl: './modal-dialog.component.scss',
+})
+export class ModalComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+}
