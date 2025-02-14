fb4157d385ffd10cb011d11cdfc3108de9771deb
Israel <israelquisbert.dev@gmail.com>
2025-02-13 20:58:56 -0400

    Updated unit testing

--- src/app/shared/components/modal-dialog/modal-dialog.component.spec.ts
+++ src/app/shared/components/modal-dialog/modal-dialog.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatDialogRef } from '@angular/material/dialog';
 import { ModalComponent } from './modal-dialog.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 describe('ModalComponent', () => {
   let component: ModalComponent;
@@ -8,7 +12,35 @@ describe('ModalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ModalComponent],
+      imports: [
+        ModalComponent,
+        MatButtonModule,
+        MatIconModule,
+        NoopAnimationsModule,
+      ],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: jasmine.createSpyObj('MatDialogRef', ['close']),
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            title: 'Test Title',
+            message: 'Test Message',
+            isSuccess: true,
+            success: {
+              details: 'Test Success Details',
+            },
+            error: {
+              title: 'Error Title',
+              message: 'Error Message',
+              showMessage: true,
+              details: ['Error detail 1', 'Error detail 2'],
+            },
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ModalComponent);
