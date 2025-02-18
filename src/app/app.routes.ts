37f217f2f3e4f68673f50ce8a7c7de253b7bac3a
ARojasA <adrian.rojas.arevalo@gmail.com>
2025-02-18 14:22:38 -0400

    Updated routes to skip login page to redirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './features/login/login.component';
 import { LayoutComponent } from './shared/components/layout/layout.component';
 import { ProfileComponent } from './features/profile/profile.component';
 import { CosmicLatteComponent } from './features/cosmic-latte/cosmic-latte.component';
@@ -7,7 +6,6 @@ import { HeatMapComponent } from './features/reports/heat-map.component';
 import { ImportAnswersComponent } from './features/import-answers/import-answers.component';
 import { ImportStudentsComponent } from './features/import-students/import-students.component';
 import { canActivateAuthRole } from './shared/guards/auth-role.guard';
-import { authGuard } from './shared/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'profile', pathMatch: 'full' },
--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -2,7 +2,7 @@ import { inject } from '@angular/core';
 import { CanActivateChildFn, Router } from '@angular/router';
 import Keycloak from 'keycloak-js';
 
-export const authGuard: CanActivateChildFn = (childRoute, state) => {
+export const authGuard: CanActivateChildFn = () => {
   const router = inject(Router);
   const keycloak = inject(Keycloak);
   const isUserLoggedIn = keycloak.authenticated; // Replace with actual logic to check if user is logged in
