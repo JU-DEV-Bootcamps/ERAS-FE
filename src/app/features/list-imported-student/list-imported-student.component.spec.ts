595761fc68ef975c2f2525093cc6232caa58944c
clareshenry <henry.clares@jala.university>
2025-02-18 17:03:14 -0400

    refactor: update tests

--- src/app/features/list-imported-student/list-imported-student.component.spec.ts
+++ src/app/features/list-imported-student/list-imported-student.component.spec.ts
@@ -1,15 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListImportedStudentComponent } from './list-imported-student.component';
+import { ImportStudentService } from '../../core/services/import-students.service';
+
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { of } from 'rxjs';
 
 describe('ListImportedStudentComponent', () => {
   let component: ListImportedStudentComponent;
   let fixture: ComponentFixture<ListImportedStudentComponent>;
+  let mockService: jasmine.SpyObj<ImportStudentService>;
 
   beforeEach(async () => {
+    mockService = jasmine.createSpyObj('ImportStudentService', ['getData']);
+    mockService.getData.and.returnValue(of({ items: [], count: 0 }));
+
     await TestBed.configureTestingModule({
       imports: [ListImportedStudentComponent],
+      providers: [
+        { provide: ImportStudentService, useValue: mockService },
+        provideNoopAnimations(),
+        provideHttpClientTesting(),
+      ],
     }).compileComponents();
+
+    component = TestBed.inject(ListImportedStudentComponent);
 
     fixture = TestBed.createComponent(ListImportedStudentComponent);
     component = fixture.componentInstance;
