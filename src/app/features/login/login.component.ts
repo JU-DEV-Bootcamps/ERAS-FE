1a81fe4c43b95f498355991d6fce39611724d9b5
ARojasA <adrian.rojas.arevalo@gmail.com>
2025-02-18 14:08:26 -0400

    fixed login css and missed close tag

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatButton],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
