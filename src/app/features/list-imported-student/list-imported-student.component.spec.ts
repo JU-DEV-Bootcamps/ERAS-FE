f15887f4e79d2bb226060fede4649dab75ac38c1
clareshenry <henry.clares@jala.university>
2025-02-18 08:20:21 -0400

    feat: add table with paginator for imported students

--- src/app/features/list-imported-student/list-imported-student.component.spec.ts
+++ src/app/features/list-imported-student/list-imported-student.component.spec.ts
@@ -1,15 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListImportedStudentComponent } from './list-imported-student.component';
+<<<<<<< HEAD
 import { ImportStudentService } from '../../core/services/import-students.service';
 
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { of } from 'rxjs';
+=======
+>>>>>>> 9c7a2ab (feat: add table with paginator for imported students)
 
 describe('ListImportedStudentComponent', () => {
   let component: ListImportedStudentComponent;
   let fixture: ComponentFixture<ListImportedStudentComponent>;
+<<<<<<< HEAD
   let mockService = jasmine.createSpyObj('ImportStudentService', ['getData']);
 
   beforeEach(async () => {
@@ -22,11 +26,20 @@ describe('ListImportedStudentComponent', () => {
         provideNoopAnimations(),
         provideHttpClientTesting(),
       ],
+=======
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ListImportedStudentComponent],
+>>>>>>> 9c7a2ab (feat: add table with paginator for imported students)
     }).compileComponents();
 
     fixture = TestBed.createComponent(ListImportedStudentComponent);
     component = fixture.componentInstance;
+<<<<<<< HEAD
     mockService = TestBed.inject(ImportStudentService);
+=======
+>>>>>>> 9c7a2ab (feat: add table with paginator for imported students)
     fixture.detectChanges();
   });
 
