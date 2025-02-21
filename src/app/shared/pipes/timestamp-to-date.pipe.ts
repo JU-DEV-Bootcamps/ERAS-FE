9c656b7b5d44914d88c87bdd9afda880ef59a69c
Ram <ramiro.vargas@jala.university>
2025-02-21 02:59:05 -0400

    feat(polls): add polls by last n days

--- src/app/shared/pipes/timestamp-to-date.pipe.ts
+++ src/app/shared/pipes/timestamp-to-date.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'timestampToDate'
+  name: 'timestampToDate',
 })
 export class TimestampToDatePipe implements PipeTransform {
   transform(value: string | null): string {
