d4f08f63e002ddb2f48c82f480756fdf47b807be
clareshenry <henry.clares@jala.university>
2025-02-25 22:16:55 -0400

    refactor: update modal risk students detail

---
+++ src/app/core/constants/heat-map.ts
@@ -0,0 +1,9 @@
+export const RISK_COLORS = {
+  1: '#008000',
+  2: '#3CB371',
+  3: '#F0D722',
+  4: '#FFA500',
+  default: '#FF0000',
+};
+
+export type RiskColorType = keyof typeof RISK_COLORS;
--- src/app/features/heat-map/modal-risk-students-detail/modal-risk-students-detail.component.ts
+++ src/app/features/heat-map/modal-risk-students-detail/modal-risk-students-detail.component.ts
@@ -1,18 +1,27 @@
-import { Component, inject, model, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {
-  MAT_DIALOG_DATA,
   MatDialogActions,
   MatDialogContent,
   MatDialogRef,
   MatDialogTitle,
 } from '@angular/material/dialog';
 import {
+  Components,
   ComponentValueType,
   RiskStudentDetailType,
 } from '../types/risk-students-detail.type';
 import { HeatMapService } from '../../../core/services/heat-map.service';
 import { MatButtonModule } from '@angular/material/button';
-import { MatChipsModule } from '@angular/material/chips';
+import { MatSelectModule } from '@angular/material/select';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { RISK_COLORS, RiskColorType } from '../../../core/constants/heat-map';
 
 @Component({
   selector: 'app-modal-risk-students-detail',
@@ -21,29 +30,70 @@ import { MatChipsModule } from '@angular/material/chips';
     MatDialogTitle,
     MatDialogContent,
     MatDialogActions,
-    MatChipsModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatProgressBarModule,
+    MatTableModule,
   ],
   templateUrl: './modal-risk-students-detail.component.html',
   styleUrl: './modal-risk-students-detail.component.scss',
 })
 export class ModalRiskStudentsDetailComponent implements OnInit {
   readonly dialogRef = inject(MatDialogRef<ModalRiskStudentsDetailComponent>);
-  readonly data = inject<{ component: ComponentValueType }>(MAT_DIALOG_DATA);
-  readonly component = model(this.data.component);
+
+  columns = ['studentId', 'studentName', 'riskLevel'];
 
   heatMapService = inject(HeatMapService);
 
   riskStudentsDetail: RiskStudentDetailType[] = [];
 
+  components = [
+    {
+      name: 'Académico',
+      value: Components.ACADEMIC,
+    },
+    {
+      name: 'Individual',
+      value: Components.INDIVIDUAL,
+    },
+    {
+      name: 'Familiar',
+      value: Components.FAMILIAR,
+    },
+    {
+      name: 'Económico',
+      value: Components.SOCIO_ECONOMIC,
+    },
+  ];
+
+  form = new FormGroup({
+    component: new FormControl<ComponentValueType | null>(
+      null,
+      Validators.required
+    ),
+  });
+
+  isLoading = false;
+
   ngOnInit(): void {
-    this.heatMapService
-      .getStudentHeatMapDetails(this.component())
-      .subscribe(data => {
-        this.riskStudentsDetail = data;
-      });
+    this.form.valueChanges.subscribe(formValue => {
+      this.findStudentsByComponent(formValue.component!);
+    });
   }
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  findStudentsByComponent(component: ComponentValueType) {
+    this.isLoading = true;
+    this.heatMapService.getStudentHeatMapDetails(component).subscribe(data => {
+      this.isLoading = false;
+      this.riskStudentsDetail = data;
+    });
+  }
+
+  getColorRisk(riskLevel: RiskColorType) {
+    return RISK_COLORS[riskLevel] || RISK_COLORS.default;
   }
 }
