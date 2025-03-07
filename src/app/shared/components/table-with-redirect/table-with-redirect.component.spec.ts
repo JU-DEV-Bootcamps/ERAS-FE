3db7b9282dfd0acc59345bb7878007a662298347
ManuelMonroy91 <Manuel.Monroy@jala.university>
2025-03-07 01:06:25 -0400

    feat: create componen list with redirect

---
+++ src/app/shared/components/table-with-redirect/table-with-redirect.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TableWithRedirectComponent } from './table-with-redirect.component';
+
+describe('TableWithRedirectComponent', () => {
+  let component: TableWithRedirectComponent;
+  let fixture: ComponentFixture<TableWithRedirectComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TableWithRedirectComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TableWithRedirectComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/components/table-with-redirect/table-with-redirect.component.ts
@@ -0,0 +1,59 @@
+import { TitleCasePipe } from '@angular/common';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-table-with-redirect',
+  standalone: true,
+  imports: [
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatIconModule,
+    TitleCasePipe,
+    CommonModule,
+    MatCardModule,
+    RouterModule,
+  ],
+  templateUrl: './table-with-redirect.component.html',
+  styleUrls: ['./table-with-redirect.component.scss'],
+})
+export class TableWithRedirectComponent implements OnInit {
+  @Input() columns: string[] = [];
+  @Input() dataSource: Record<string, unknown>[] = [];
+  @Input() uuidField = '';
+  @Input() redirectPath = '';
+  @Input() itemStorage = '';
+
+  isMobile = false;
+  totalItems = 0;
+
+  constructor(private router: Router) {}
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: UIEvent): void {
+    const target = event.target as Window;
+    this.isMobile = target.innerWidth < 768;
+  }
+
+  ngOnInit() {
+    this.isMobile = window.innerWidth < 768;
+    this.totalItems = this.dataSource.length;
+  }
+
+  redirectToDetail(item: Record<string, unknown>): void {
+    const uuid = item[this.uuidField];
+    if (uuid && this.redirectPath && this.itemStorage) {
+      localStorage.setItem(this.itemStorage, uuid as string);
+      this.router.navigate([this.redirectPath]);
+    } else {
+      console.error('Missing uuid, redirectPath, or itemStorage');
+    }
+  }
+}
