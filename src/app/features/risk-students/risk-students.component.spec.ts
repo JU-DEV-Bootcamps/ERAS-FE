49f3a712378c83cdb269d4b1f272c6cc0a5a65d4
MauroFonsecaJala <mauro.fonseca@jalasoft.com>
2025-04-16 09:42:16 -0300

    refactor(risk-students): create component, update layout and route

---
+++ src/app/features/risk-students/risk-students.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RiskStudentsComponent } from './risk-students.component';
+
+describe('RiskStudentsComponent', () => {
+  let component: RiskStudentsComponent;
+  let fixture: ComponentFixture<RiskStudentsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RiskStudentsComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RiskStudentsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/risk-students/risk-students.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-risk-students',
+  imports: [],
+  templateUrl: './risk-students.component.html',
+  styleUrl: './risk-students.component.css'
+})
+export class RiskStudentsComponent {
+
+}
