682a162f3b558a624f5d4a9077074985f6873d32
Henry Clares <henry.clares@jalasoft.com>
2025-05-06 15:19:06 -0400

    feat: add empty data component

---
+++ src/app/shared/components/empty-data/empty-data.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-data',
+  imports: [],
+  templateUrl: './empty-data.component.html',
+  styleUrl: './empty-data.component.css',
+})
+export class EmptyDataComponent {
+  @Input() title = '';
+  @Input() description = 'No data loaded.';
+}
