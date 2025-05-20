47eb87737921125d2a63ba7420fd5a19db476848
Israel Quisbert Anti <Israel.Quisbert@jalasoft.com>
2025-05-20 17:29:50 -0400

    fix(ERAS-147): corrected the test for the breadcrumb component

--- src/app/shared/components/breadcrumb/breadcrumb.component.spec.ts
+++ src/app/shared/components/breadcrumb/breadcrumb.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BreadcrumbComponent } from './breadcrumb.component';
+import { BreadcrumbsService } from '../../../core/services/breadcrumbs.service';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('BreadcrumbComponent', () => {
   let component: BreadcrumbComponent;
@@ -9,6 +11,13 @@ describe('BreadcrumbComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [BreadcrumbComponent],
+      providers: [
+        BreadcrumbsService,
+        {
+          provide: ActivatedRoute,
+          useValue: { snapshot: { paramMap: of({}) } },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(BreadcrumbComponent);
