b56f73297a24350a0868b09745c805c120e85757
MauroFonsecaJala <mauro.fonseca@jalasoft.com>
2025-05-21 11:44:41 -0300

    refactor(csv-service): update tests

--- src/app/core/services/exports/csv.service.spec.ts
+++ src/app/core/services/exports/csv.service.spec.ts
@@ -1,13 +1,36 @@
 import { TestBed } from '@angular/core/testing';
-
 import { CsvService } from './csv.service';
+import { BaseExportService } from './base-export.service';
+import { BreadcrumbsService } from '../breadcrumbs.service';
 
-describe('CsvService', () => {
+fdescribe('CsvService', () => {
   let service: CsvService;
+  let baseExportServiceSpy: jasmine.SpyObj<BaseExportService>;
+  let breadcrumbsServiceSpy: jasmine.SpyObj<BreadcrumbsService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    baseExportServiceSpy = jasmine.createSpyObj('BaseExportService', [
+      'downloadTextFile',
+    ]);
+    breadcrumbsServiceSpy = jasmine.createSpyObj('BreadcrumbsService', [
+      'getLabels',
+    ]);
+
+    TestBed.configureTestingModule({
+      providers: [
+        CsvService,
+        { provide: BaseExportService, useValue: baseExportServiceSpy },
+        { provide: BreadcrumbsService, useValue: breadcrumbsServiceSpy },
+      ],
+    });
+
     service = TestBed.inject(CsvService);
+    // Override the inherited method with the spy
+    (
+      service as unknown as {
+        downloadTextFile: typeof baseExportServiceSpy.downloadTextFile;
+      }
+    ).downloadTextFile = baseExportServiceSpy.downloadTextFile;
   });
 
   it('should be created', () => {
