41858019db24a212ae6cc34788f8e5648271cca4
Jose Luis Guevara <joseluis.guevara.private@gmail.com>
2025-09-16 20:05:25 -0400

    feat(ERAS-385): integration end up for add detail pages

--- src/app/modules/supports-referrals/services/referrals.mapper.service.ts
+++ src/app/modules/supports-referrals/services/referrals.mapper.service.ts
@@ -1,27 +1,25 @@
 import { inject, Injectable } from '@angular/core';
 
-import { forkJoin, map, of } from 'rxjs';
-import Keycloak from 'keycloak-js';
+import { forkJoin, map, Observable, of } from 'rxjs';
 
 import { Profile } from '@core/models/profile.model';
 import { StudentResponse } from '@core/models/student-request.model';
 import { StudentService } from '@core/services/api/student.service';
 import { PagedResult } from '@core/services/interfaces/page.type';
-import { RESTReferral } from '../models/referrals.interfaces';
+import { Referral, RESTReferral } from '../models/referrals.interfaces';
 
 enum StatusCode {
-  'created',
-  'submitted',
-  'on-hold',
-  'in-progress',
-  'completed',
+  'Created',
+  'Submitted',
+  'On Hold',
+  'In Progress',
+  'Completed',
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class ReferralsMapperService {
-  keycloak = inject(Keycloak);
   studentService = inject(StudentService);
 
   mapReferrals(referrals: PagedResult<RESTReferral>, profile: Profile) {
@@ -32,25 +30,9 @@ export class ReferralsMapperService {
         profile: {},
       });
     }
-    const referralRequests = referrals.items.map((referral: RESTReferral) => {
-      const studentRequests = referral.studentIds.map((studentId: number) =>
-        this.studentService.getStudentDetailsById(studentId, {
-          page: 0,
-          pageSize: 10,
-        })
-      );
-
-      return forkJoin(studentRequests).pipe(
-        map(students => ({
-          ...referral,
-          service: referral.juService.name,
-          professional: referral.assignedProfessional.name,
-          status: StatusCode[referral.status],
-          submitter: profile.firstName,
-          student: this.getStudents(students),
-        }))
-      );
-    });
+    const referralRequests = referrals.items.map((referral: RESTReferral) =>
+      this._getStudents(referral, profile)
+    );
 
     return forkJoin(referralRequests).pipe(
       map(referralsPopulated => ({
@@ -61,7 +43,33 @@ export class ReferralsMapperService {
     );
   }
 
-  getStudents(students: StudentResponse[]): string {
+  mapReferral(referral: RESTReferral, profile: Profile): Observable<Referral> {
+    return this._getStudents(referral, profile);
+  }
+
+  _getStudents(referral: RESTReferral, profile: Profile): Observable<Referral> {
+    const studentRequests = referral.studentIds.map((studentId: number) =>
+      this.studentService.getStudentDetailsById(studentId, {
+        page: 0,
+        pageSize: 10,
+      })
+    );
+
+    return forkJoin(studentRequests).pipe(
+      map(students => ({
+        comment: referral.comment,
+        date: referral.date,
+        id: referral.id,
+        professional: referral.assignedProfessional.name,
+        service: referral.juService.name,
+        status: StatusCode[referral.status],
+        student: this._getStudentsNames(students),
+        submitter: profile.firstName,
+      }))
+    );
+  }
+
+  _getStudentsNames(students: StudentResponse[]): string {
     return students
       .map((student: StudentResponse) => student.entity.name)
       .join(', ');
--- src/app/shared/components/buttons/eras-button/eras-button.component.ts
+++ src/app/shared/components/buttons/eras-button/eras-button.component.ts
@@ -2,7 +2,7 @@ import { Component, input, output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  selector: 'eras-button',
+  selector: 'app-eras-button',
   imports: [MatButtonModule],
   templateUrl: './eras-button.component.html',
 })
