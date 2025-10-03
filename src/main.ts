902659e497529c1519d408f6dc8e3ac349d863a4
Jose Luis Guevara <joseluis.guevara.private@gmail.com>
2025-10-03 09:56:57 -0400

    feat(ERAS-596): enable production env variable and remove console to test

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
-
-// TODO: REMOVE AFTER CONFIRM ENV PROD IS WORKING
-console.log('PRODUCTION MODE:', environment.production);
