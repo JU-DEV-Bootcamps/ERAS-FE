d8a1aa41e241f03473b11495933ed22bbf09273c
Sebastian Beltran <sebastian.beltran@jalasoft.com>
2026-04-13 09:13:35 -0500

    fix: fix selected-checkbox tests

--- src/app/modules/students/students-list/selected-checkbox/selected-checkbox.component.spec.ts
+++ src/app/modules/students/students-list/selected-checkbox/selected-checkbox.component.spec.ts
@@ -1,10 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SelectedCheckboxComponent } from './selected-checkbox.component';
+import { SelectableModel } from '@core/models/common/selectable.model';
 
 describe('SelectedCheckboxComponent', () => {
-  let component: SelectedCheckboxComponent;
-  let fixture: ComponentFixture<SelectedCheckboxComponent>;
+  let component: SelectedCheckboxComponent<SelectableModel>;
+  let fixture: ComponentFixture<SelectedCheckboxComponent<SelectableModel>>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
