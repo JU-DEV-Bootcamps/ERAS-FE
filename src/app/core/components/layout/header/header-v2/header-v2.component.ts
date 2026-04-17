c985c79ab6d4422d488f0f121649bc346fb4de05
judithmarg-dev <Judith.Paco@jalasoft.com>
2026-04-17 16:09:21 -0400

    feat(FE-782): add header profile information

--- src/app/core/components/layout/header/header-v2/header-v2.component.ts
+++ src/app/core/components/layout/header/header-v2/header-v2.component.ts
@@ -1,13 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
 import { UserMenuComponent } from '../../user-menu/user-menu.component';
+import { UserDataService } from '@core/services/access/user-data.service';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header-v2',
-  imports: [MatToolbar, UserMenuComponent],
+  imports: [MatToolbar, UserMenuComponent, MatIcon],
   templateUrl: './header-v2.component.html',
   styleUrl: './header-v2.component.scss',
 })
 export class HeaderV2Component {
   @Input() sectionTitle!: string | null | undefined;
+  private readonly userData = inject(UserDataService);
+  user = this.userData.user;
 }
