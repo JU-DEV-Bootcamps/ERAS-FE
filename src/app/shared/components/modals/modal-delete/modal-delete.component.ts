82ddd3764a6eef325c25a19b69210298d565384b
judithmarg-dev <Judith.Paco@jalasoft.com>
2026-05-18 21:32:30 -0400

    feat: add modal to delete items

---
+++ src/app/shared/components/modals/modal-delete/modal-delete.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-delete',
+  imports: [],
+  templateUrl: './modal-delete.component.html',
+  styleUrl: './modal-delete.component.scss',
+})
+export class ModalDeleteComponent {}
