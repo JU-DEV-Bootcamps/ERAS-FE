5639c6cc0819682422745419fa9fee030454bfd6
judithmarg-dev <Judith.Paco@jalasoft.com>
2026-06-29 22:30:34 -0400

    refactor: add metadata as data in routes

--- src/app/modules/home-v2/home.routes.ts
+++ src/app/modules/home-v2/home.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeContainerComponent } from './home-container.component';
+import { ROUTE_METADATA } from '@core/utils/routing/route-metadata';
 
 export const HOME_ROUTES: Routes = [
   {
     path: '',
     component: HomeContainerComponent,
-    data: {
-      breadcrumb: 'Home',
-      headerTitle: 'Home',
-    },
+    data: ROUTE_METADATA.HOME,
   },
 ];
