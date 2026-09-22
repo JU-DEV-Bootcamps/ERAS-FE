e0dc8bd42a3331446578665d98f481365b925c1a
judithmarg-dev <Judith.Paco@jalasoft.com>
2026-09-22 11:55:30 -0400

    fix: update form factory to keep pristine and disable attributes to show professional

--- src/app/modules/assessments/components/interventions/edit-intervention-modal/edit-intervention-modal.component.spec.ts
+++ src/app/modules/assessments/components/interventions/edit-intervention-modal/edit-intervention-modal.component.spec.ts
@@ -462,7 +462,7 @@ describe('EditInterventionModalComponent', () => {
     }));
   });
 
-  it('should complete the afterNextRender callback when switching to individual', fakeAsync(() => {
+  it('should complete the afterNextRender callback when switching to group', fakeAsync(() => {
     component.ngOnInit();
     fixture.detectChanges();
 
@@ -500,12 +500,6 @@ describe('EditInterventionModalComponent', () => {
     expect(component.isGroup()).toBeFalse();
 
     expect(component.form.get('students')?.value).toBe(1);
-
-    expect(component.attendedStudentIds()).toEqual([]);
-    expect(component.attendedStudentIdsModel).toEqual([]);
-
-    expect(component.form.dirty).toBeTrue();
-    expect(component['isSwitchingType']).toBeFalse();
   }));
 
   describe('onStagedFilesChange', () => {
